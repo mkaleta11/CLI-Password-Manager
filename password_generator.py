c3659c680e75f0707b0ea264c692e09de99052ff
juve-kaleta <maciekk2005@gmail.com>
2025-08-03 17:39:48 +0200

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,51 @@
+### This is a simple password generator using my own algorithm and using the secrets and string module
+
+import secrets
+import string
+
+def fisher_yates_shuffle(arr):
+    '''
+    Shuffling an array using the Fisher Yates algorithm combined with secrets module.
+    '''
+    for i in range(len(arr)-1, 0, -1):
+        j = secrets.randbelow(i+1)
+        arr[i], arr[j] = arr[j], arr[i]
+
+
+def password_generator():
+    '''
+    Generating a strong password containing 12-18 letters.
+    With at least 1 digit, letter and special character.
+    '''
+    passwordArray = []
+    letters = string.ascii_letters
+    numbers = string.digits
+    specialCharacters = list('!@#$%^&*_+=-[],.?><')
+    
+    lengthOfPassword = secrets.choice(range(12,19))
+
+    # Include every type of character 
+    passwordArray.append(secrets.choice(letters))
+    passwordArray.append(secrets.choice(numbers))
+    passwordArray.append(secrets.choice(specialCharacters))
+
+    # Adding the remaining characters
+    for i in range(lengthOfPassword-3):
+        randomIndex = secrets.choice([0,1,2])
+
+        if randomIndex == 0:
+            passwordArray.append(secrets.choice(letters))
+
+        elif randomIndex == 1:
+            passwordArray.append(secrets.choice(numbers))
+
+        else:
+            passwordArray.append(secrets.choice(specialCharacters))
+    
+    fisher_yates_shuffle(passwordArray)
+
+    return ''.join(passwordArray)
+    
+
+if __name__ == '__main__':
+    print(f'Generated password: {password_generator()}')
